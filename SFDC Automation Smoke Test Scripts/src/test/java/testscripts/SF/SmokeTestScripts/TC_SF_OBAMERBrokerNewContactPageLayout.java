125becbe4c7709aee23baff7580521c91cb83e20
Vishnuvardhan <vishnuvardhan.bommisetty@cbre.com>
2017-05-04 11:47:58 -0500

    Adding Smoke Test Scripts

---
+++ SFDC Automation Smoke Test Scripts/src/test/java/testscripts/SF/SmokeTestScripts/TC_SF_OBAMERBrokerNewContactPageLayout.java
@@ -0,0 +1,48 @@
+package testscripts.SF.SmokeTestScripts;
+
+import org.openqa.selenium.Platform;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import com.cognizant.Craft.CRAFTTestCase;
+import com.cognizant.framework.IterationOptions;
+import com.cognizant.framework.selenium.Browser;
+import com.cognizant.framework.selenium.ExecutionMode;
+import com.cognizant.framework.selenium.SeleniumTestParameters;
+
+import com.cognizant.Craft.*;
+/**
+ * Test Script for validating the Account Name in the New Contacts page layout
+ * 
+ * @author Vishnuvardhan
+ *
+ */
+
+public class TC_SF_OBAMERBrokerNewContactPageLayout extends CRAFTTestCase
+{
+	@Test(dataProvider = "SmokeTestScripts")
+	public void runTC_SF_OBAMERBrokerNewContactPageLayout(String testInstance, ExecutionMode executionMode, Platform platform
+			/*MobileToolName mobileToolName,
+			MobileExecutionPlatform executionPlatform, String deviceName*/)
+	{
+		SeleniumTestParameters testParameters = new SeleniumTestParameters(currentScenario, currentTestcase);
+		testParameters.setCurrentTestDescription("Test for validating the Account Name in the New Contacts page layout");
+		testParameters.setIterationMode(IterationOptions.RUN_ONE_ITERATION_ONLY);
+		testParameters.setBrowser(Browser.CHROME);
+		testParameters.setExecutionMode(executionMode);
+		testParameters.setPlatform(platform);
+		DriverScript driverScript = new DriverScript(testParameters);
+		driverScript.driveTestExecution();
+		tearDownTestRunner(testParameters, driverScript);
+	}
+	
+	@DataProvider(name = "SmokeTestScripts", parallel = false)
+	public Object[][] dataTC2() {
+		return new Object[][] { { "Instance1", ExecutionMode.LOCAL, Platform.WINDOWS /*ExecutionMode.LOCAL,
+				MobileToolName.APPIUM, MobileExecutionPlatform.IOS,
+				"4d005cb2c4938197"*/ },
+			};
+	}
+}
+
+
+
