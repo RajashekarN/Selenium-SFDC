55e99826f1edfb8202d8c77fbff7a58229fdad2d
Bommisetty, Vishnuvardhan @ CBRE Contractor <Vishnuvardhan.Bommisetty@cbre.com>
2017-06-20 21:57:32 +0500

    Updated TC_SF_API_Admin_FetchRecord.java

--- SFDC_Automation_Regression_Test_Scripts/src/test/java/testscripts/SF/SmokeTest_API/TC_SF_API_Admin_FetchRecord.java
+++ SFDC_Automation_Regression_Test_Scripts/src/test/java/testscripts/SF/SmokeTest_API/TC_SF_API_Admin_FetchRecord.java
@@ -16,10 +16,10 @@ import com.cognizant.Craft.*;
  * @author Vishnuvardhan
  *
  */
-public class TC_SF_API_AdminFetchRecord extends CRAFTTestCase
+public class TC_SF_API_Admin_FetchRecord extends CRAFTTestCase
 {
 	@Test(dataProvider = "SmokeTestScripts")
-	public void runTC_SF_API_AdminFetchRecord(String testInstance, ExecutionMode executionMode, Platform platform
+	public void runTC_SF_API_Admin_FetchRecord(String testInstance, ExecutionMode executionMode, Platform platform
 			/*MobileToolName mobileToolName,
 			MobileExecutionPlatform executionPlatform, String deviceName*/)
 	{
