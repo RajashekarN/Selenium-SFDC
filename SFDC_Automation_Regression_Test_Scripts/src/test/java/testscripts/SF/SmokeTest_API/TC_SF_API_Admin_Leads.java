051eb4e8fc777e60eab5c5a1afa436c6a9d71943
Bommisetty, Vishnuvardhan @ CBRE Contractor <Vishnuvardhan.Bommisetty@cbre.com>
2017-06-20 22:15:30 +0500

    Updated TC_SF_API_Admin_Leads.java

--- SFDC_Automation_Regression_Test_Scripts/src/test/java/testscripts/SF/SmokeTest_API/TC_SF_API_Admin_Leads.java
+++ SFDC_Automation_Regression_Test_Scripts/src/test/java/testscripts/SF/SmokeTest_API/TC_SF_API_Admin_Leads.java
@@ -19,7 +19,7 @@ import com.cognizant.Craft.*;
 public class TC_SF_API_Admin_Leads extends CRAFTTestCase
 {
 	@Test(dataProvider = "SmokeTestScripts")
-	public void runTC_SF_API_Admin_Leads.java(String testInstance, ExecutionMode executionMode, Platform platform
+	public void runTC_SF_API_Admin_Leads(String testInstance, ExecutionMode executionMode, Platform platform
 			/*MobileToolName mobileToolName,
 			MobileExecutionPlatform executionPlatform, String deviceName*/)
 	{
