e6a9a6b053d8804a76dde82ce0d717e57872cc5e
US\SBitra <swapna.bitra@cbre.com>
2018-06-19 15:16:32 -0500

    Added fixed with respect to Contacts functionality

--- SFDC_Automation_Regression_Test_Scripts/src/test/java/supportLibraries/SF_UtilityFunctions.java
+++ SFDC_Automation_Regression_Test_Scripts/src/test/java/supportLibraries/SF_UtilityFunctions.java
@@ -3,6 +3,8 @@ package supportLibraries;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
@@ -552,8 +554,21 @@ public class SF_UtilityFunctions extends ReusableLibrary {
 		}		
 	}
 	
-	/**
-	 * This method selects checkbox on Standard Popup
+    /**
+    * This method selects the pick list values from the drop down list
+    * 
+     * @author Vishnuvardhan
+    */
+    
+    public void selectValueFromDropdownList(List<WebElement> list, String Text) {
+           for(WebElement element: driver.findElements(By.xpath("//ul[@class='scrollable']/li"))) {
+                  if(element.getText().equals(Text)) {
+                        Utility_Functions.xClick(driver, element, true);
+                  }
+           }
+    }
+    
+    /* This method selects checkbox on Standard Popup
 	 * @author Swapna
 	 */
 	public void selectCheckBox(String checkBoxFieldName){
@@ -569,18 +584,213 @@ public class SF_UtilityFunctions extends ReusableLibrary {
 		}
 	}
 	
-    /**
-    * This method selects the pick list values from the drop down list
-    * 
-     * @author Vishnuvardhan
-    */
-    
-    public void selectValueFromDropdownList(List<WebElement> list, String Text) {
-           for(WebElement element: driver.findElements(By.xpath("//ul[@class='scrollable']/li"))) {
-                  if(element.getText().equals(Text)) {
-                        Utility_Functions.xClick(driver, element, true);
-                  }
-           }
-    }
+	/*
+	 * *******************************************************************
+	 *  UI Functions to validate page layout  
+	 * ******************************************************************
+	 */
+	
+
+	/**
+	 * This function validates Action Buttons on a Page Layout
+	 * @param List<String> ActionButtonName(Ex: Edit,Clone) 
+	 * @author Swapna
+	 */
+	public void validatePageActionButtons(List<String> ExpectedActions){
+		try{
+			By ActionsXpath = By.xpath("//li/a[@role='button' or @role = 'menuitem']/div");
+			By showMoreActionXpath = By.xpath("//span[text()='Show more actions']/ancestor::a");
+			driver.navigate().refresh();
+			Utility_Functions.xWaitForElementPresent(driver, showMoreActionXpath, 8);
+			Utility_Functions.xClick(driver, driver.findElement(showMoreActionXpath), true);
+			Utility_Functions.xWaitForElementPresent(driver, ActionsXpath, 3);
+			List<WebElement> ActionBtns= driver.findElements(ActionsXpath);
+			List<String> ActualActions = new ArrayList<String>();
+			for(int i =0;i<ActionBtns.size();i++){
+				ActualActions.add(ActionBtns.get(i).getText());
+			}
+			Assert.assertEqualsNoOrder(ActualActions.toArray(),ExpectedActions.toArray());
+			System.out.println("Verified Actions are present on the page"+ ExpectedActions);
+		}catch(Exception e){
+			System.out.println("Error occured on verify Page Actions "+e.getMessage());
+		}
+	}
+	
+	/**
+	 * This function validates Header Labels on a Page Layout
+	 * @param List<String> HeaderFieldName(Ex: Title, CloseDate, etc.) 
+	 * @author Swapna
+	 */
+	public void validatePageHeaderFields(List<String> ExpectedHeaderFields ){
+		try{
+			By headerFieldsXpath = By.xpath("//header//li/p[1]");
+			driver.navigate().refresh();
+			Utility_Functions.xWaitForElementPresent(driver, headerFieldsXpath, 5);
+			List<WebElement> headerFields= driver.findElements(headerFieldsXpath);
+			List<String> ActualheaderFields = new ArrayList<String>();
+			for(int i =0;i<headerFields.size();i++){
+				ActualheaderFields.add(headerFields.get(i).getText());
+			}
+			System.out.println(ExpectedHeaderFields+" == "+ActualheaderFields);
+			Assert.assertEqualsNoOrder(ActualheaderFields.toArray(), ExpectedHeaderFields.toArray());
+			System.out.println("Verified header fields are present on the page "+ ExpectedHeaderFields);
+		}catch(Exception e){
+			System.out.println("Error occured on verify Page header fields "+e.getMessage());
+		}
+	}
+	
+	/**
+	 * This function validates Related Tab RelatedLists on a Page Layout
+	 * 
+	 * @param Map<String, List<String>>
+	 *     RelatedTabName(Ex: RELATED) and ComponentName(Ex: Team Member,Installments etc..))
+	 * @author Swapna
+	 */
+	public void validatePageRelatedSection(Map<String, List<String>> pgRelatedSections){
+	 try{
+		String RelatedTabName = null;
+		for (Map.Entry<String, List<String>> entry : pgRelatedSections.entrySet()) {
+			Utility_Functions.xScrollWindowTop(driver);
+			RelatedTabName = entry.getKey();
+			System.out.println("Related Tab Name :: " + RelatedTabName);
+			/*By relatedTabXpath = By.xpath("//span[text()='"+RelatedTabName+"']");
+			Utility_Functions.xWaitForElementPresent(driver, relatedTabXpath, 3);
+			Utility_Functions.xClick(driver, driver.findElement(relatedTabXpath), true);
+			*/
+			selectRelatedTab(RelatedTabName);
+			if (entry.getValue() != null && entry.getValue().size() > 0) {
+				for (String sectionName : entry.getValue()) {
+					Utility_Functions.xScrollWindowTop(driver);
+					By sectionXpath = By.xpath("//h2//span[text()= '"+sectionName+"']");
+					Utility_Functions.xScrollWindowToElement(driver,driver.findElement(sectionXpath));
+				}
+			}
+			System.out.println("Verified Related sections : " + entry.getValue() + " are present under the Related Tab : "
+					+ RelatedTabName + " ::::passed");
+		 }
+	   }catch(Exception e){
+		 System.out.println("Error validating related tab related lists ::"+e.getMessage());
+	   }
+   }
+	
+	
+	/**
+	 * This function validates Right panel Components on a Page Layout
+	 * 
+	 * @param Map<String, List<String>>
+	 *       ComponentTabName(Ex: Activity) and ComponentName(Ex: New Task))
+	 * @author Swapna
+	 */
+	
+	public void validatePageActivityComponents(Map<String, List<String>> pgRelatedSections){
+		 try{
+			 String ComponentTabName = null;
+			 for (Map.Entry<String, List<String>> entry : pgRelatedSections.entrySet()) {
+				Utility_Functions.xScrollWindowTop(driver);
+				ComponentTabName = entry.getKey();
+				System.out.println("Right Panel Component Tab Name :: " + ComponentTabName);
+				By CompTabXpath = By.xpath("//span[text()='"+ComponentTabName+"']");
+				Utility_Functions.xWaitForElementPresent(driver, CompTabXpath, 3);
+				Utility_Functions.xClick(driver, driver.findElement(CompTabXpath), true);
+				if (entry.getValue() != null && entry.getValue().size() > 0) {
+					for (String CompName : entry.getValue()) {
+					  By CompXpath = By.xpath("//span[text()= '"+CompName+"']");
+						try{
+							Utility_Functions.xWaitForElementPresent(driver, CompXpath, 3);
+							driver.findElement(CompXpath);
+						}catch(Exception e){
+							CompXpath = By.xpath("//h2[text()= '"+CompName+"']");
+							Utility_Functions.xWaitForElementPresent(driver, CompXpath, 3);
+							driver.findElement(CompXpath);
+						}
+					}
+				}
+				System.out.println("Verified right panel components : " + entry.getValue() + " are present under : "
+						+ ComponentTabName + " ::::passed");
+			 }
+		 }catch(Exception e){
+			 System.out.println("Error validating right panel components ::"+e.getMessage());
+	   }
+  	}
+	/**
+	 * This function validates Field Labels under All sections by sectionName in Contact Page
+	 * Layout
+	 * 
+	 * @param Map<String, List<String>>
+	 *            (SectionName and FieldNames)
+	 * @author Swapna
+	 */
+	public void verifyAllSectionElementsInPage(Map<String, List<String>> mapObj) {
+	 try{	
+		WebElement section = null;
+		String sectionName = null;
+		Map<String, List<String>> mapObject = mapObj;
+		for (Map.Entry<String, List<String>> entry : mapObject.entrySet()) {
+			Utility_Functions.xScrollWindowTop(driver);
+			sectionName = entry.getKey();
+			System.out.println("Section Name :: " + entry.getKey());
+			System.out.println("Field Name  ::" + entry.getValue());
+			if (entry.getValue() != null && entry.getValue().size() > 0) {
+				for (String fieldName : entry.getValue()) {
+					By Field_Under_SectionInDetail = By.xpath("//span[text()='" + sectionName
+							+ "']/ancestor::h3/following-sibling::div//span[string-length(text()) > 0 and text()='"
+							+ fieldName + "']");
+					try {
+						 try{
+						    Utility_Functions.xScrollWindowToElement(driver,driver.findElement(Field_Under_SectionInDetail));
+						 }catch(Exception e){
+							 Field_Under_SectionInDetail = By.xpath("//span[text()='"+sectionName+"']/ancestor::h3/following-sibling::div//a[string-length(text()) > 0 and text()='"+fieldName+"']");
+							 Utility_Functions.xScrollWindowToElement(driver,
+								    	driver.findElement(Field_Under_SectionInDetail));
+						 }
+						} catch (Exception e) {
+							By Field_Under_SectionInPopup = By.xpath("//span[text()='" + sectionName
+								+ "']/parent::h3/following-sibling::div//span[string-length(text()) > 0 and text()='"
+								+ fieldName + "']");
+							Utility_Functions.xScrollWindowToElement(driver,
+							driver.findElement(Field_Under_SectionInPopup));
+						}
+				}
+				System.out.println("Verified Field Names : " + entry.getValue() + " are present under the section : "
+						+ sectionName + " ::::passed");
+			} else {
+				By sectionName_InDetail = By.xpath("//span[text()='" + sectionName + "']/ancestor::h3//span");
+				try {
+					Utility_Functions.xScrollWindowToElement(driver, driver.findElement(sectionName_InDetail));
+					section = driver.findElement(sectionName_InDetail);
+				} catch (Exception e) {
+					By sectionName_InPopup = By.xpath("//span[text()='" + sectionName + "']/parent::h3/span");
+					Utility_Functions.xScrollWindowToElement(driver, driver.findElement(sectionName_InPopup));
+					section = driver.findElement(sectionName_InPopup);
+				}
+			  System.out.println("Verified section is present on the Contact Page :::"+ section.getText());
+			}
+		}
+	  }catch(Exception e){
+		 System.out.println("Error validating page elements under each section ::"+e.getMessage());
+	   }
+	}
+
+	/**
+	 * returns the Page Header text from the details page
+	 * 
+	 * @author Swapna
+	 * @return 
+	 *
+	 */
+	public String getPageHeaderTitle(String ObjectName) { 
+		By detailHeaderSec_TextTitle= By.xpath("//p[(text()='"+ObjectName+"')]//following-sibling::h1//span");
+		By detailHeaderSec_LinkTextTitle = By.xpath("//p[text()='"+ObjectName+"']/following-sibling::h1//a");
+		String headerString = null;
+		Utility_Functions.xScrollWindowTop(driver);
+		try{
+			Utility_Functions.xWaitForElementPresent(driver, detailHeaderSec_TextTitle, 5);
+			headerString = driver.findElement(detailHeaderSec_TextTitle).getText();
+		}catch(Exception e){
+			Utility_Functions.xWaitForElementPresent(driver, detailHeaderSec_LinkTextTitle, 5);
+			headerString = driver.findElement(detailHeaderSec_LinkTextTitle).getText();			
+		}
+		return headerString;
+	}
 
 }
